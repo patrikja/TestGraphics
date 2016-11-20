215467cf34c3d628c7a7972fd1aa849f1c2787f1
Patrik Jansson <patrik.ja@gmail.com>
2016-11-20 22:11:17 +0100

    Minor refactoring

--- test-gloss/src/Lib.hs
+++ test-gloss/src/Lib.hs
@@ -1,23 +1,22 @@
 module Lib
     ( test
     ) where
-import Graphics.Gloss.Data.Picture
--- import Graphics.Gloss.Data.Bitmap
-
 import Graphics.Gloss
-import Codec.BMP
 import System.Environment
 
-run fileName
- = do   picture@(Bitmap width height _ _)
-                <- loadBMP fileName
+run fileName = do
+  picture@(Bitmap width height _ _)
+    <- loadBMP fileName
+  let offset = (10,  10)
+  display
+    (InWindow fileName (width, height) offset)
+    black $ Pictures
+      [ rectangleSolid (fromIntegral width) (fromIntegral height)
+      , picture]
 
-        display (InWindow fileName (width, height) (10,  10))
-                black $ Pictures
-                  [ rectangleSolid (fromIntegral width) (fromIntegral height)
-                  , picture]
-
--- Perhaps later: check out https://hackage.haskell.org/package/gloss-raster-accelerate-1.9.0.0/docs/src/Graphics-Gloss-Accelerate-Raster-Array.html
+-- Perhaps later: check out
+--    https://hackage.haskell.org/package/gloss-raster-1.10.2.4/docs/Graphics-Gloss-Raster-Array.html
+-- or https://hackage.haskell.org/package/gloss-raster-accelerate-1.9.0.0/docs/src/Graphics-Gloss-Accelerate-Raster-Array.html
 
 -- Look at simulate or play
 --   https://hackage.haskell.org/package/gloss-1.10.2.3/docs/Graphics-Gloss.html#v:simulte
